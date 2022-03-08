a69f579d5b449ae0056cc702e55d40f6f569abf2
Vadim Constantin <vad.constantin@gmail.com>
2022-03-08 16:54:14 +0100

    first commit

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,18 @@
+class PokemonsController < ApplicationController
+
+  require 'json'
+  require 'open-uri'
+
+  def index
+     if params[:query].present?
+      api_url = "https://pokeapi.co/api/v2/pokemon/#{params[:query].downcase}"
+      @pokemon = JSON.parse(URI.open(api_url).read)
+    else
+       redirect_to :root
+    end
+  end
+
+  def show
+  end
+
+end
